aa8d2d2d02bcca77ac42efac542dd58a70b3b76e
codecomet07 <codecomet7@gmail.com>
2025-02-18 10:04:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -48,4 +48,5 @@ def predict():
     return jsonify({'error': 'Could not process the audio'}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True,host ='0.0.0.0')
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True,host ='0.0.0.0',port=port)
