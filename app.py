b9fb2a2a6c821d8cbe2241fe02467b747ccf2726
codecomet07 <codecomet7@gmail.com>
2025-03-22 19:21:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -49,4 +49,4 @@ def predict():
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 10000))  # Use Render's default port
-    app.run(debug=True, host='0.0.0.0', port=port)
+    app.run(debug=True, host='100.20.92.101', port=port)
