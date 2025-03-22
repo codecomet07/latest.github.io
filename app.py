f587e8ba908b251e5f3eb260bff2b48b204c3e24
codecomet07 <codecomet7@gmail.com>
2025-03-22 18:49:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -48,5 +48,5 @@ def predict():
     return jsonify({'error': 'Could not process the audio'}), 500
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True,host ='0.0.0.0',port=port)
+    port = int(os.environ.get("PORT", 10000))  # Use Render's default port
+    app.run(debug=True, host='0.0.0.0', port=port)
